c2ca661fa8da0a5317108bc9a2bba99ba6911931
Liam Manley <wkmanle@s1096724ca.srn.sandia.gov>
2026-07-20 10:42:48 -0700

    test with directory specified

--- docs/examples/csv_transformation/test_data_transformation.sh
+++ docs/examples/csv_transformation/test_data_transformation.sh
@@ -1,15 +1,24 @@
 #!/bin/zsh
+CSV_INPUT="test_predictions.csv"
 
-[[ -f "test_predictions.csv" ]] || echo "test_predictions.csv missing";
+[[ -f "$CSV_INPUT" ]] || echo "${CSV_INPUT} missing";
 
 # test with no directory specified
 uv run python data_transformation.py test_predictions.csv input_evm input_rhom >/dev/null
 [[ -d "training_results_input_evm_input_rhom" ]] || echo "Failed to create default directory"
-
-[[ -f "training_results_input_evm_input_rhom/data.pickle" ]] || echo "Failed to create data.pickle"
 
 for file in "data.pickle" "train_output.pickle" "sm.pickle"; do
     [[ -f "training_results_input_evm_input_rhom/$file" ]] || echo "${file} was not created"
 done
 
 rm -r training_results_input_evm_input_rhom
+
+# test with directory specified
+uv run python data_transformation.py test_predictions.csv input_evm input_rhom specified/output >/dev/null
+[[ -d "specified/output" ]] || echo "Failed to create specified directory"
+
+for file in "data.pickle" "train_output.pickle" "sm.pickle"; do
+    [[ -f "specified/output/$file" ]] || echo "${file} was not created"
+done
+
+rm -r specified
