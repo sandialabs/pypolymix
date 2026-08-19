c705c3244916a5c835a783708ed9bb956576e6bb
Liam Manley <wkmanle@sandia.gov>
2026-08-19 13:59:46 -0700

    unit test DeterministicGroup method return shapes

--- tests/parameter_groups/test_deterministic.py
+++ tests/parameter_groups/test_deterministic.py
@@ -13,6 +13,10 @@ def test_sample_parameters_returns_tensor(deterministic_group):
     parameters = deterministic_group.sample_parameters(num_samples=3)
     assert isinstance(parameters, torch.Tensor)
 
+def test_sample_parameters_returns_correct_shape(deterministic_group):
+    parameters = deterministic_group.sample_parameters(num_samples=3)
+    assert parameters.shape == torch.Size([3,2])
+
 def test_variational_distribution_RuntimeError(deterministic_group):
     with pytest.raises(RuntimeError):
         deterministic_group.variational_distribution()
@@ -20,3 +24,7 @@ def test_variational_distribution_RuntimeError(deterministic_group):
 def test_distribution_loss_returns_tensor(deterministic_group):
     distribution_loss = deterministic_group.distribution_loss()
     assert isinstance(distribution_loss, torch.Tensor)
+
+def test_distribution_loss_returns_scaler(deterministic_group):
+    distribution_loss = deterministic_group.distribution_loss()
+    assert distribution_loss.ndim == 0
